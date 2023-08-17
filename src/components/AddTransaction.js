5db31b195fb06edb5c3e7b08560e0e27a8d66309
YennhiDUT <91811893+YennhiDUT@users.noreply.github.com>
2023-08-17 22:55:27 +0700

    reducer

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -1,21 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 
 const AddTransaction = () => {
+  const [text, setText] = useState("");
+  const [mount, setAmount] = useState(0);
   return (
     <>
       <h3>Add new transaction</h3>
       <form>
         <div className="form-control">
           <label htmlFor="text">Text</label>
-          <input type="text" placeholder="Enter text..." />
+          <input
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+            type="text"
+            placeholder="Enter text..."
+          />
         </div>
+
         <div className="form-control">
           <label htmlFor="amount">
             Amount <br />
             (negative - expense, positive - income)
           </label>
-          <input type="number" placeholder="Enter amount..." />
+          <input
+            value={mount}
+            onChange={(e) => {
+              setAmount(e.target.value);
+            }}
+            type="number"
+            placeholder="Enter amount..."
+          />
         </div>
+
         <button className="btn">Add transaction</button>
       </form>
     </>
